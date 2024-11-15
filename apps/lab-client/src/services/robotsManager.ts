fa2bf783c4bfaf72ec05923d4d17839002abc663
Conor Hayes <cwoodhayes@gmail.com>
2024-11-15 16:52:06 -0500

    set up robotsmanager to just automatically make & connect with a
    videochat

--- apps/lab-client/src/services/robotsManager.ts
+++ apps/lab-client/src/services/robotsManager.ts
@@ -53,10 +53,12 @@ class RobotsManager {
 
     public static async getInstance(callbacks: RobotsManagerCallbacks): Promise<RobotsManager> {
         if (!RobotsManager.singleton) {
-            // todo add real callbacks
-            // for now we're just testing the bridge master
-            // in the future we'll use incoming connections to create new robot drivers
-            RobotsManager.singleton = new RobotsManager(await RTCBridgeMaster.getInstance({}), callbacks);
+            // todo add real callbacks for errors
+            RobotsManager.singleton = new RobotsManager(await RTCBridgeMaster.getInstance({
+                onPeerConnected: (connection: RTCPeerConnection, peerId: string) => RobotsManager.singleton?.onPeerConnected(connection, peerId),
+                onSignalingDisconnect: () => { throw new Error("signaling disconnected"); },
+                onSignalingError: (error: Error) => { throw error; },
+            }), callbacks);
             await RobotsManager.singleton.setup();
         }
         return RobotsManager.singleton;
@@ -65,6 +67,25 @@ class RobotsManager {
     private async setup() {
         // start looking for robots or something idk we'll see if we end up needing anything here
         console.log('Setting up robots manager...');
+    }
+
+    private async onPeerConnected(connection: RTCPeerConnection, peerId: string) {
+        console.log('Peer connected (handling in robots manager):', peerId);
+
+        // hardcode setting up the first video chat robot in the map for now
+        // so we don't have to implement complex state management yet
+        // we are assuming there already exists a robot created via connectRobot
+        let driver = Array.from(this.driversById.values()).find(driver => driver instanceof VideoChatDriver);
+        if (!driver) {
+            // just connect a new one
+            await this.connectRobot(VideoChatDriver);
+            driver = this.driversById.values().next().value;
+        }
+        driver?.connectUser(connection);
+    }
+
+    private async onPeerDisconnected(peerId: string) {
+        console.log('Peer disconnected (handling in robots manager):', peerId);
     }
 
     get rtcMaster(): RTCBridgeMaster {
